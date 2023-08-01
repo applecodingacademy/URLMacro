6fa408bd94c247c9cdd71a1ea0a4f4791342ca52
Julio César Fernández <juliocesar.fernandez@gabhel.com>
2023-08-01 13:46:41 +0200

    0.0.2

--- Sources/URLMacro/URLMacro.swift
+++ Sources/URLMacro/URLMacro.swift
@@ -1,6 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+
 /// A macro that produces both a value and a string containing the
 /// source code that generated the value. For example,
 ///
@@ -9,3 +11,6 @@
 /// produces a tuple `(x + y, "x + y")`.
 @freestanding(expression)
 public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "URLMacroMacros", type: "StringifyMacro")
+
+@freestanding(expression)
+public macro URL(_ stringLiteral: String) -> URL = #externalMacro(module: "URLMacroMacros", type: "URLMacro")
